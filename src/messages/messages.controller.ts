c77b054caa6b59272043badda51ec7443b60e76e
Gabriel <sollero.gms@gmail.com>
2024-05-07 12:02:17 -0300

    feat: create MessagesService and use in MessagesController

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,14 +1,28 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DONT DO ON REAL APPS
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
-  createMessages(@Body() body: CreateMessageDto) {}
+  createMessages(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {}
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
 }
