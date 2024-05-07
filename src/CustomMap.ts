b0841738506a853cc485535fa8c29be4470722cd
Gabriel <sollero.gms@gmail.com>
2024-05-07 08:12:32 -0300

    feat: add CustomMap class and add user and company to map

---
+++ src/CustomMap.ts
@@ -0,0 +1,29 @@
+/// <reference types="@types/google.maps" />
+
+interface Mappable {
+  location: google.maps.LatLngLiteral;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+  }
+}
