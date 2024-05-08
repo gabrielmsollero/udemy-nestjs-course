993eeecfc743efb82b8002fd9bdbd6cb927e5979
Gabriel <sollero.gms@gmail.com>
2024-05-08 08:46:16 -0300

    feat: create DiskService

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(public powerService: PowerService) {}
+
+  getData() {
+    console.log('Drawing 20W');
+    this.powerService.supplyPower(20);
+
+    return 'data';
+  }
+}
