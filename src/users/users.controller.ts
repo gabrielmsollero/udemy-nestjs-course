4660b1c7492b34495a41d62f10d5be73133647a3
Gabriel <sollero.gms@gmail.com>
2024-05-09 18:35:34 -0300

    feat: create auth guard and use in whoami route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { AuthService } from './auth.service';
@@ -18,6 +19,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -28,6 +30,7 @@ export class UsersController {
   ) {}
 
   @Get('whoami')
+  @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user: User, @Session() session: any) {
     return this.usersService.findOne(session.userId);
   }
