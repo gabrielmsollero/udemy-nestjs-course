95ee806daa89dafa8e5f24f22ddc3b1a48b965be
Gabriel <sollero.gms@gmail.com>
2024-05-09 17:31:13 -0300

    feat: create signup and signin routes in users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [AuthService, UsersService],
 })
 export class UsersModule {}
